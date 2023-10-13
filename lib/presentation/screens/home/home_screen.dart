7d60bab930230e21e4908bb003bf41827c7d7b79
Jhon Carreño <jhon7car@gmail.com>
2023-10-13 18:55:29 -0300

    confi go_router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -58,7 +59,9 @@ class _CustomListTitle extends StatelessWidget {
         //   ),
         // );
 
-        Navigator.pushNamed(context, menuItem.link);
+        // Navigator.pushNamed(context, menuItem.link);
+
+        context.push(menuItem.link);
       },
     );
   }
