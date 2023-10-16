3ee92d2b4c34cdf2d0716be93789ca7865ef2355
Jhon Carreño <jhon7car@gmail.com>
2023-10-16 12:08:00 -0300

    progress indicator

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -8,6 +8,43 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Progress Indicator'),
+      ),
+      body:const _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30,),
+          Text('Circular Progress Indicator'),
+          SizedBox(height: 10),
+          CircularProgressIndicator(strokeWidth: 2, backgroundColor: Colors.black45,),
+          SizedBox(height: 20),
+          Text('Progress Indicator Controlado'),
+          SizedBox(height: 10),
+          _ControlledProgressIndicator(),
+
+        ],
+      )
+      );
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
